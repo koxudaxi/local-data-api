f25d3d9adf938f12d2b51a7e6d789a8019102b76
Koudai Aono <koxudaxi@gmail.com>
2019-06-24 14:21:39 +0900

    fix to execute with parameters (#12)
    * fix to execute with parameters
    
    * fix type hint
    
    * add unittest for valid_value

---
+++ tests/test_models.py
@@ -0,0 +1,15 @@
+
+from unittest import TestCase
+
+from local_data_api.models import SqlParameter, Field
+
+
+class TestSqlParameter(TestCase):
+    def test_valid_field(self) -> None:
+        self.assertEqual(SqlParameter(name='abc', value=Field(stringValue='abc')).valid_value, 'abc')
+        self.assertEqual(SqlParameter(name='abc', value=Field(blobValue='abc')).valid_value, 'abc')
+        self.assertEqual(SqlParameter(name='abc', value=Field(doubleValue=0.1)).valid_value, 0.1)
+        self.assertEqual(SqlParameter(name='abc', value=Field(isNull=True)).valid_value, None)
+        self.assertEqual(SqlParameter(name='abc', value=Field(longValue=123)).valid_value, 123)
+        self.assertEqual(SqlParameter(name='abc', value=Field(longValue=123)).valid_value, 123)
+        self.assertEqual(SqlParameter(name='abc', value=Field()).valid_value, None)
