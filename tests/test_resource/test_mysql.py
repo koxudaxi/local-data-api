b6f218a31a809d3ae7b828b547a54f874d575a64
Koudai Aono <koxudaxi@gmail.com>
2019-07-03 13:32:47 +0900

    add unittest

--- tests/test_resource/test_mysql.py
+++ tests/test_resource/test_mysql.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from local_data_api.resources import MySQL
 
 
-class TestSQL(TestCase):
+class TestMySQL(TestCase):
     def test_create_connection_maker(self):
         with patch('local_data_api.resources.mysql.pymysql.connect') as mock_connect:
             connection_maker = MySQL.create_connection_maker(host='127.0.0.1', port=3306, user_name='root',
@@ -14,3 +14,8 @@ class TestSQL(TestCase):
             connection_maker()
         mock_connect.assert_called_once_with(auto_commit=True, host='127.0.0.1', password='pass',
                                              port=3306, user='root')
+
+        with patch('local_data_api.resources.mysql.pymysql.connect') as mock_connect:
+            connection_maker = MySQL.create_connection_maker()
+            connection_maker()
+        mock_connect.assert_called_once_with()
