75dbebc47cbfd0760f266313018cc3e9fd8fa587
Koudai Aono <koxudaxi@gmail.com>
2019-07-08 12:16:49 +0900

    add unittest

--- tests/test_resource/test_jdbc/test_mysql.py
+++ tests/test_resource/test_jdbc/test_mysql.py
@@ -32,12 +32,27 @@ class TestResource(TestCase):
         connection_mock.cursor.side_effect = [cursor_mock]
         cursor_mock.description = ''
         cursor_mock.rowcount = 1
-        cursor_mock.fetchone.side_effect = [[1]]
+        cursor_mock.fetchone.side_effect = [[0]]
         dummy = MySQLJDBC(connection_mock)
         self.assertEqual(dummy.execute("insert into users values (1, 'abc')"),
-                         ExecuteStatementResponse(numberOfRecordsUpdated=1, generatedFields=[Field(longValue=1)]))
+                         ExecuteStatementResponse(numberOfRecordsUpdated=1, generatedFields=[]))
         cursor_mock.execute.assert_has_calls([call('SELECT LAST_INSERT_ID(NULL)'),
                                               call("insert into users values (1, 'abc')"),
+                                              call('SELECT LAST_INSERT_ID()')])
+        cursor_mock.close.assert_called_once_with()
+
+    def test_execute_insert_with_generated_field(self):
+        connection_mock = Mock()
+        cursor_mock = Mock()
+        connection_mock.cursor.side_effect = [cursor_mock]
+        cursor_mock.description = ''
+        cursor_mock.rowcount = 1
+        cursor_mock.fetchone.side_effect = [[1]]
+        dummy = MySQLJDBC(connection_mock)
+        self.assertEqual(dummy.execute("insert into users (name) values ('abc')"),
+                         ExecuteStatementResponse(numberOfRecordsUpdated=1, generatedFields=[Field(longValue=1)]))
+        cursor_mock.execute.assert_has_calls([call('SELECT LAST_INSERT_ID(NULL)'),
+                                              call("insert into users (name) values ('abc')"),
                                               call('SELECT LAST_INSERT_ID()')])
         cursor_mock.close.assert_called_once_with()
 
@@ -47,10 +62,10 @@ class TestResource(TestCase):
         connection_mock.cursor.side_effect = [cursor_mock]
         cursor_mock.description = ''
         cursor_mock.rowcount = 1
-        cursor_mock.fetchone.side_effect = [[1]]
+        cursor_mock.fetchone.side_effect = [[0]]
         dummy = MySQLJDBC(connection_mock)
         self.assertEqual(dummy.execute("insert into users values (:id, :name)", {'id': 1, 'name': 'abc'}),
-                         ExecuteStatementResponse(numberOfRecordsUpdated=1, generatedFields=[Field(longValue=1)]))
+                         ExecuteStatementResponse(numberOfRecordsUpdated=1, generatedFields=[]))
         cursor_mock.execute.assert_has_calls([call('SELECT LAST_INSERT_ID(NULL)'),
                                               call("insert into users values (1, 'abc')"),
                                               call('SELECT LAST_INSERT_ID()')])
@@ -176,6 +191,35 @@ class TestResource(TestCase):
                 dummy.execute("select * from users")
             except Exception as e:
                 self.assertEqual(e.message, 'cause_error_message')
+                raise
+        cursor_mock.execute.assert_has_calls([call('SELECT LAST_INSERT_ID(NULL)'),
+                                              call('select * from users')])
+        cursor_mock.close.assert_called_once_with()
+
+    def test_execute_exception_3(self):
+        connection_mock = Mock()
+        cursor_mock = Mock()
+        connection_mock.cursor.side_effect = [jaydebeapi.DatabaseError()]
+        dummy = MySQLJDBC(connection_mock, transaction_id='123')
+        with self.assertRaises(BadRequestException):
+                dummy.execute("select * from users")
+        cursor_mock.close.assert_not_called()
+
+    def test_execute_exception_4(self):
+        connection_mock = Mock()
+        cursor_mock = Mock()
+        error = jaydebeapi.DatabaseError('error')
+        inner_error = Mock()
+        inner_error.args = ['inner_error_message']
+        error.args = [inner_error]
+        cursor_mock.execute.side_effect = [0, error]
+        connection_mock.cursor.side_effect = [cursor_mock]
+        dummy = MySQLJDBC(connection_mock, transaction_id='123')
+        with self.assertRaises(BadRequestException):
+            try:
+                dummy.execute("select * from users")
+            except Exception as e:
+                self.assertEqual(e.message, 'inner_error_message')
                 raise
         cursor_mock.execute.assert_has_calls([call('SELECT LAST_INSERT_ID(NULL)'),
                                               call('select * from users')])
