af3735316bf2847da18e4fca1ddd5346deb8a31b
Koudai Aono <koxudaxi@gmail.com>
2019-07-30 16:47:08 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from setuptools import setup
 from setuptools.config import read_configuration
 
-config = read_configuration(Path(__file__).parent.joinpath('setup.cfg'))
+PROJECT_PATH = Path(__file__).parent
+config = read_configuration(PROJECT_PATH.joinpath('setup.cfg'))
 
 extras_require = {
     'setup': config['options']['setup_requires'],
@@ -14,7 +15,7 @@ extras_require = {
 extras_require['all'] = [*extras_require.values()]
 kwargs = {'extras_require': extras_require}
 
-if (Path(__file__) / '.git').exists():
+if (PROJECT_PATH / '.git').exists():
     use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
     kwargs['use_scm_version'] = use_scm_version
 
