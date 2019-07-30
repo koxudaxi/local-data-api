81550c1a1a3b6033ea7eed55c6932b0d9119cd41
Koudai Aono <koxudaxi@gmail.com>
2019-07-30 15:50:15 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+from pathlib import Path
+
 from setuptools import setup
+from setuptools.config import read_configuration
+
+config = read_configuration(Path(__file__).parent.joinpath('setup.cfg'))
+
+extras_require = {
+    'setup': config['options']['setup_requires'],
+    'test': config['options']['tests_require'],
+    **config['options']['extras_require'],
+}
+
+extras_require['all'] = [*extras_require.values()]
+use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
+
 setup(
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    extras_require=extras_require,
+    use_scm_version=use_scm_version,
 )
