bcfab53339493d006bcec3b0fbe3c92115ef5399
Koudai Aono <koxudaxi@gmail.com>
2019-07-30 16:13:34 +0900

    fix Dockerfile

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@ extras_require = {
 }
 
 extras_require['all'] = [*extras_require.values()]
-use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
+kwargs = {'extras_require': extras_require}
+
+if (Path(__file__) / '.git').exists():
+    use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
+    kwargs['use_scm_version'] = use_scm_version
 
 setup(
-    extras_require=extras_require,
-    use_scm_version=use_scm_version,
+    **kwargs
 )
