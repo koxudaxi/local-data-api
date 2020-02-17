f551574b156f3edb6b5668761de2705540d85bb1
Koudai Aono <koxudaxi@gmail.com>
2020-02-17 16:45:02 +0900

    fix begin_statement

--- local_data_api/main.py
+++ local_data_api/main.py
@@ -34,7 +34,7 @@ def execute_sql(request: ExecuteSqlRequest) -> None:
 
 @app.post("/BeginTransaction", response_model=BeginTransactionResponse)
 def begin_statement(request: BeginTransactionRequest) -> BeginTransactionResponse:
-    resource: Resource = get_resource(request.resourceArn, request.secretArn)
+    resource: Resource = get_resource(request.resourceArn, request.secretArn, database=request.database)
     transaction_id: str = resource.begin()
 
     return BeginTransactionResponse(transactionId=transaction_id)
